97f44d927e3159b284d67ae4ae117d6138dcefed
Jagdish <2021ucp1385@mnit.ac.in>
2024-07-15 07:27:10 +0530

    JWT Authentication

--- Videos/E-COM-API/server.js
+++ Videos/E-COM-API/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import productRouter from './src/features/product/product.routes.js';
 import userRouter from './src/features/user/user.routes.js';
-import basicAuthorizer from './src/middlewares/basicAuth.middleware.js';
+import jwtAuth from './src/middlewares/jwt.middleware.js';
 // 2. Create Server
 const server = express();
 server.use(express.json());
@@ -12,7 +12,11 @@ server.use(express.json());
 server.use(bodyParser.json());
 // for all requests related to product, redirect to product routes.
 // localhost:3200/api/products
-server.use('/api/products', basicAuthorizer, productRouter);
+server.use(
+    '/api/products',
+    jwtAuth,
+    productRouter
+);
 server.use('/api/users', userRouter);
 // 3. Default request handler
 server.get('/', (req, res) => {
---
+++ Videos/E-COM-API/src/features/user/user.routes.js
@@ -0,0 +1,23 @@
+// Manage routes/paths to ProductController
+
+// 1. Import express.
+import express from 'express';
+import UserController from './user.controller.js';
+
+// 2. Initialize Express router.
+const userRouter = express.Router();
+const userController = new UserController();
+
+// All the paths to the controller methods.
+
+userRouter.post(
+    '/signup',
+    userController.signUp
+);
+
+userRouter.post(
+    '/signin',
+    userController.signIn
+);
+
+export default userRouter;
