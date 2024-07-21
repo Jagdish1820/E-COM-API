348029da004e1b37762b825021314bb99b8f06c3
Jagdish <2021ucp1385@mnit.ac.in>
2024-07-21 16:27:12 +0530

    Deleting Cart Item

--- Videos/E-COM-API/src/features/cartitems/cartitems.controller.js
+++ Videos/E-COM-API/src/features/cartitems/cartitems.controller.js
@@ -12,6 +12,19 @@ export class CartItemsController {
         const userID = req.userID;
         const items = CartItemModel.get(userID);
         return res.status(200).send(items);
+    }
 
+    delete(req, res)
+    {
+        const userID = req.userID;
+        const cartItemID = req.params.id;
+        const error = CartItemModel.delete(
+            cartItemID, 
+            userID
+        );
+        if(error){
+            return res.status(404).send(error);
+        }
+        return res.status(200).send('Cart Item is removed');
     }
 }
