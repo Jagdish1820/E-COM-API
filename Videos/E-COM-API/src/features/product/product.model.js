bb70781b212e2d854e06fe2565b3ffa339e3d26c
Jagdish <2021ucp1385@mnit.ac.in>
2024-07-29 02:41:39 +0530

    Customizing Error Handling Middleware

--- Videos/E-COM-API/src/features/product/product.model.js
+++ Videos/E-COM-API/src/features/product/product.model.js
@@ -1,3 +1,4 @@
+import { ApplicationError } from '../../error-handler/applicationError.js';
 import UserModel from '../user/user.model.js';
 
 export default class ProductModel {
@@ -57,14 +58,14 @@ export default class ProductModel {
     );
     if(!user){
       // user-defined error.
-      throw new Error("User not found"); 
+      throw new ApplicationError("User not found", 404); 
     }
 
     // Validate Product
     const product = products.find(
       (p) => p.id == productID);
       if(!product){
-        throw new Error("Product not found"); 
+        throw new ApplicationError("Product not found", 400); 
       }
 
       // 2. Check if there are any ratings and if not then add ratings array.
